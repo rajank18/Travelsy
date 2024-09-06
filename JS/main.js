b7cb9d52c8f3ab0e2acf5e435de1ca3429c339c4
Rajan Kanzariya <rajankanzariya1806@gmail.com>
2024-09-06 14:49:25 +0530

    review section

--- JS/main.js
+++ JS/main.js
@@ -11,3 +11,27 @@ document.querySelector('.arrow').addEventListener('click', function() {
         alert("Please enter a city name.");
     }
 });
+
+(function ($) {
+
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+})(jQuery);
