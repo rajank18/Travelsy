fb9566d423d8f732f696e00c5e2b72bd5a1c3002
Manav-User <manavmajmudar01@gmail.com>
2024-09-07 16:22:32 +0530

    review page

--- JS/review.js
+++ JS/review.js
@@ -1,7 +1,8 @@
 document.getElementById('submit').addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('message').innerHTML = `
-        Your review has been submitted
-        <span>Go back to <a href="/Travelsy/HTML/index.html">Home Page</a></span>
+        Your review has been submitted.
+        <br>
+        <button type="button" class="btn btn-outline-success" id="home"><a href="/Travelsy/HTML/index.html">Home</a></button>
     `;
 });
