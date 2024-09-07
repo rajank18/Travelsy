e61cbf3e8de6858b1056f1ca30becb469f0d13af
kd-debug <23dit007@charusat.edu.in>
2024-09-07 16:55:56 +0530

    login

---
+++ login/server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+const path = require('path');
+require('dotenv').config();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+    if (err) { console.error('Error connectiong the database:', err);
+         } 
+    console.log('Connected to database...');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/register', (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 8);
+
+    const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+    db.query(query, [username, email, hashedPassword], (err, result) => {
+        if (err) {
+            console.error('Error inserting into users:', err);
+            res.render('register', { error: 'Registration failed. Please try again.' });
+        }
+        res.render('success', { message: 'Registration successful' });
+    });
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    const query = 'SELECT * FROM users WHERE email = ?';
+    db.query(query, [email], (err, result) => {
+        if (err){ console.error('Error logging in:', err);
+            res.render('register', { error: 'Registration failed. Please try again.' }); } 
+        if (result.length > 0) {
+            const user = result[0];
+            const isMatch = bcrypt.compareSync(password, user.password);
+
+            if (isMatch) {
+                res.render('success', { message: 'Login successful' });
+            } else {
+                res.render('login', { error: 'Incorrect password' });
+            }
+        } else {
+            res.render('login', { error: 'Email not found' });
+        }
+    });
+});
+app.listen(3111);
